0e1ba208a31fec389cf77fea44e3ce6fb0d354ee
Joke de Buhr <joke@xckk.de>
2022-09-16 13:04:08 +0200

    feat: istioctl completions
    Signed-off-by: Joke de Buhr <joke@xckk.de>

---
+++ init.zsh
@@ -0,0 +1,8 @@
+(( ${+commands[istioctl]} || ${+commands[asdf]} && ${+functions[_direnv_hook]} )) && () {
+
+  local command=${commands[istioctl]:-"$(${commands[asdf]} which kn 2> /dev/null)"}
+  [[ -z $command ]] && return 1
+
+  local compfile=$1/functions/_istioctl
+  [[ ! -e $compfile || $compfile -ot $command ]] && $command completion zsh >| $compfile
+} ${0:h}
